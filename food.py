52994e8a2638f2379d5dfe5bd30b5ac8223bd2bf
liduvo <hasbey945@gmail.com>
2023-09-05 11:56:38 +0300

    Third Commit

--- food.py
+++ food.py
@@ -13,8 +13,8 @@ class Food(Turtle):
 
     def refresh(self):
 
-        random_x = random.randint(-300, 300)
-        random_y = random.randint(-300, 300)
+        random_x = random.randint(-270, 270)
+        random_y = random.randint(-270, 270)
         self.goto(random_x, random_y)
 
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from snake import Snake
 from turtle import Screen, Turtle
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(600, 600)
@@ -11,6 +12,7 @@ screen.title("My Snake Game")
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -26,6 +28,19 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 
 screen.exitonclick()
